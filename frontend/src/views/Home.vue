4230d0dfd6d20016eec0ebcbd376068496b01ef1
QuadDepo <ryan@qdds.nl>
2021-06-20 19:12:27 +0200

    Add Main pages

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,74 @@
 <template>
-  Home
+  <div class="joke">
+    <div class="row"> 
+      <search-wrapper @onSearch="getJokes" />
+    </div>
+    <div v-if="!loading && !error" class="row"> 
+      <Joke v-for="joke in jokes" :key="joke.id" :joke="joke" />
+      <p v-if="jokes.length === 0 && searches > 0">
+        No Jokes! Even Chuck Norris is disapointed.
+      </p>
+    </div>
+    <div v-if="error" class="row"> 
+      <p>Oeps something went wrong!</p>
+    </div>
+    <div v-if="loading" class="row"> 
+      <p>Loading....</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapActions, mapGetters, mapState } from 'vuex';
+import SearchWrapper from "../components/SearchWrapper";
+import Joke from "../components/Joke"
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    'search-wrapper': SearchWrapper,
+    Joke,
+  },
+  computed: {
+    ...mapGetters(["jokes"]),
+    ...mapState(["loading","error", "searches"]),
+  },
+
+  methods: {
+    ...mapActions([
+      "fetchJokes"
+    ]),
+
+    getJokes(random, isQuerySearch) {
+      console.log(isQuerySearch);
+      const payload = {...this.$route.query}
+      // convert string to bool
+
+      if (random) {
+        this.fetchJokes()
+        return;
+      }
+
+      if (!isQuerySearch) {
+        delete payload.query;
+      }
+
+      if (isQuerySearch) {
+        delete payload.categories;
+      }
+
+      this.fetchJokes(payload)
+    }
+  },
+
+  mounted() {
+    const { query } = this.$route;
+
+    if (!(Object.prototype.hasOwnProperty.call(query, 'categories') || Object.prototype.hasOwnProperty.call(query, 'query'))) {
+      return;
+    }
+
+    this.getJokes()
+  }
 };
 </script>
