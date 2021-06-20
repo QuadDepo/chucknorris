42002254473da9f26d8631253ceefd4d71b704ef
QuadDepo <ryan@qdds.nl>
2021-06-20 19:11:53 +0200

    Add Vuex Store

---
+++ frontend/src/core/store.js
@@ -0,0 +1,92 @@
+import { createStore } from "vuex";
+import { notify } from "@kyvg/vue3-notification";
+
+export default createStore({
+  state() {
+    return {
+      favourites: [],
+      jokes: [],
+      loading: false,
+      error: false,
+      searches: 0,
+    };
+  },
+  getters: {
+    favourites(state) {
+      return state.favourites;
+    },
+    jokes(state) {
+      return state.jokes;
+    },
+  },
+  mutations: {
+    addtoFavourites(state, payload) {
+      if (this.state.favourites.length === 5) {
+        notify({
+          title: "Chuck Norris says no!",
+          text: "You reach the maxium limit of favourite jokes",
+          type: "error",
+        });
+
+        return;
+      }
+
+      notify({
+        title: "Joke added!",
+        text: "Joke has been added to your favourites",
+      });
+
+      state.favourites.push(payload);
+    },
+    removefromFavourites(state, payload) {
+      notify({
+        title: "Jokes removed!",
+        text: "Joke has been removed to your favourites",
+        type: "warn",
+      });
+
+      state.favourites = state.favourites.filter(({ id }) => payload !== id);
+    },
+    setJokes(state, payload) {
+      state.jokes = payload;
+    },
+    setLoading(state, payload) {
+      state.loading = payload;
+    },
+    setError(state, payload) {
+      state.error = payload;
+    },
+    setSearchCount(state) {
+      state.searches = ++state.searches;
+    },
+  },
+  actions: {
+    async fetchJokes({ commit }, payload) {
+      try {
+        const queryString = new URLSearchParams(payload);
+        commit("setLoading", true);
+        commit("setError", false);
+
+        const data = await fetch(`/jokes?${queryString}`);
+        const res = await data.json();
+
+        if (data.status === 500) throw new Error(res.err);
+
+        commit("setSearchCount");
+        commit("setJokes", res);
+      } catch (error) {
+        commit("setError", error);
+
+        notify({
+          title: "Chuck Norris says no!",
+          text: error,
+          type: "error",
+        });
+
+        console.error(error);
+      } finally {
+        commit("setLoading", false);
+      }
+    },
+  },
+});
