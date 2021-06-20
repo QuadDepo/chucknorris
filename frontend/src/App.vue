13309897017cffa7834f336bbcb77e4871e7e6a3
QuadDepo <ryan@qdds.nl>
2021-06-20 19:14:20 +0200

    Add Router to App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,27 @@
 <template>
-  <router-view />
+    <div class="container">
+      <notifications />
+      <navbar />
+      <div class="row center-xs">
+        <div class="col-xs-10 col-md-10">
+          <router-view />
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
 export default {
   name: 'App',
+  components: {
+    navbar: Navbar
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .container {
+    padding: 0 0 3rem 0;
+  }
+</style>
