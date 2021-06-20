5dbc49c69f0e651228d01f7ecab0ad4c2f98db85
QuadDepo <ryan@qdds.nl>
2021-06-20 19:13:56 +0200

    Add Store and Notification to App

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,14 @@
 import { createApp } from "vue";
+import Notifications from "@kyvg/vue3-notification";
 import router from "./router";
+import store from "./core/store";
 import App from "./App";
+
+import "./assets/scss/main.scss";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(store);
+app.use(Notifications);
 app.mount("#app");
