39ae6f489cceb9b9ba0a9bfc204e1cd9564ceeb0
QuadDepo <ryan@qdds.nl>
2021-06-20 20:43:52 +0200

    Fix meta data

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,4 +2,10 @@ module.exports = {
   devServer: {
     proxy: "http://localhost:9000",
   },
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "Chuck Norris Search",
+    },
+  },
 };
