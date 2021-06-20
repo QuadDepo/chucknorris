f64cb67c10b95f40f5e933be6d6a109cd8e8fec5
QuadDepo <ryan@qdds.nl>
2021-06-20 19:15:42 +0200

    Add SearchInput Component

---
+++ frontend/src/components/SearchInput/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <input
+      :placeholder="placeholder"
+      :value="value"
+      class="search-input__input"
+      type="text"
+    />
+</template>
+
+<script>
+export default {
+  name: "search-input",
+  props: {
+    placeholder: {
+      required: false,
+      type: String,
+      default: "The dark is afraid of Chuck Norris...",
+    },
+    value: {
+      required: false,
+      type: String,
+      default: "",
+    },
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.search-input {
+  display: flex;
+  align-items: center;
+  width: 100%;
+
+  &__input {
+    width: 100%;
+    border-radius: 22px;
+    padding: 0 16px;
+    height: 44px;
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: transparent;
+    outline: none;
+    border: 1px solid #dfe1e5;
+  }
+}
+</style>
