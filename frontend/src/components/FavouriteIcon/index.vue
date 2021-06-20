dcf92ea9db1ee75377cb39e71fedce1ac82faa84
QuadDepo <ryan@qdds.nl>
2021-06-20 19:15:01 +0200

    Add FavouriteIcon Component

---
+++ frontend/src/components/FavouriteIcon/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <img v-if="!favourite" class="favourite-icon" src="assets/heart.svg" />
+  <img v-else class="favourite-icon" src="assets/heart-active.svg" />
+</template>
+
+<script>
+export default {
+  name: "FavouriteIcon",
+  props: {
+    favourite: {
+      required: false,
+      type: Boolean,
+      default: false
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .favourite-icon {
+    cursor: pointer;
+    height: 20px;
+    width: 20px;
+  }
+</style>
